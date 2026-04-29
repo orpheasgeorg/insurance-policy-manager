549a9c5e3cce2a77193c28539266eec38185a760
orpheasgeorg <orpheas21@gmail.com>
2026-04-29 11:27:43 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/org/orpheus/insurancepolicymanager/InsurancePolicyManagerApplication.java
@@ -0,0 +1,13 @@
+package org.orpheus.insurancepolicymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancePolicyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsurancePolicyManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/orpheus/insurancepolicymanager/InsurancePolicyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.orpheus.insurancepolicymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancePolicyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
